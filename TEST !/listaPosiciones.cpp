73e03f97a867924a73ed37e29e13b116064408b3
SENBO ZHOU <senzhou@ucm.es>
2025-05-06 03:23:56 +0200

    listaPosicones hecho

--- TEST !/listaPosiciones.cpp
+++ TEST !/listaPosiciones.cpp
@@ -5,20 +5,17 @@
 void inicializar(tListaPosiciones& lista_pos)
 {
 	lista_pos.cont = 0;
+	lista_pos.capacidad = MAX_LISTA;
 
-	lista_pos.lista[lista_pos.cont].posx = 0;
-	lista_pos.lista[lista_pos.cont].posy = 0;
+	lista_pos.lista = new tPosicion[lista_pos.capacidad];
 }
 
 void insertar_final(tListaPosiciones& lista_pos, int x, int y) {
-	if (lista_pos.cont < MAX_LISTA)
-	{
-		lista_pos.lista[lista_pos.cont].posx = x;
-
-		lista_pos.lista[lista_pos.cont].posy = y;
-
-		lista_pos.cont++;
-	}
+	redimensionamiento(lista_pos);
+	
+	lista_pos.lista[lista_pos.cont].posx = x;
+	lista_pos.lista[lista_pos.cont].posy = y;
+	lista_pos.cont++;
 }
 
 int longitud(const tListaPosiciones& lista_pos) {
@@ -31,4 +28,27 @@ int dame_posX(const tListaPosiciones& lista_pos, int i) {
 
 int dame_posY(const tListaPosiciones& lista_pos, int i) {
 	return lista_pos.lista[i].posy;
+}
+
+void destruye(tListaPosiciones& lista_posiciones) {
+	delete[] lista_posiciones.lista;
+
+	lista_posiciones.lista = nullptr;
+	lista_posiciones.cont = 0;
+
+}
+
+void redimensionamiento(tListaPosiciones& lista_pos) {
+	if (lista_pos.cont >= lista_pos.capacidad) {
+		int newSize = lista_pos.capacidad * 2;
+		tPosicion* vaux = new tPosicion[newSize];
+
+		for (int i = 0; i < lista_pos.cont; i++) {
+			vaux[i] = lista_pos.lista[i];
+		}
+
+		lista_pos.capacidad = newSize;
+		delete[] lista_pos.lista;
+		lista_pos.lista = vaux;
+	}
 }
--- TEST !/listaPosiciones.h
+++ TEST !/listaPosiciones.h
@@ -8,9 +8,11 @@ typedef struct {
 	int posx;
 	int posy;
 } tPosicion;
+
 typedef struct {
-	tPosicion lista[MAX_LISTA];
+	tPosicion* lista;
 	int cont;
+	int capacidad;
 } tListaPosiciones;
 
 void inicializar(tListaPosiciones& lista_pos);
@@ -18,3 +20,7 @@ void insertar_final(tListaPosiciones& lista_pos, int x, int y);
 int longitud(const tListaPosiciones& lista_pos);
 int dame_posX(const tListaPosiciones& lista_pos, int i);
 int dame_posY(const tListaPosiciones& lista_pos, int i);
+
+void destruye(tListaPosiciones& lista_posiciones);
+
+void redimensionamiento(tListaPosiciones& lista_pos);
